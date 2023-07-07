e073eacfaf5700844c4d96ab95ac3471d27725a1
Rimberse <kerimpltm@gmail.com>
2023-07-07 15:05:05 +0200

    Implement helper functions to handle Map lifecycle: Render and Destroy

--- urban-planning/src/components/LeafletMap.tsx
+++ urban-planning/src/components/LeafletMap.tsx
@@ -1,11 +1,48 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, RefObject,} from "react";
+import L, { Map } from "leaflet";
+import "leaflet/dist/leaflet.css";
 
 const LeafletMap = () => {
-  const map = useRef();
+  let mapElement: RefObject<HTMLDivElement> = useRef(null);
+  let map = useRef<Map | null>();
 
-  const init = () => {};
+  const initMap = (element: HTMLElement) => {
+    const map: Map = L.map(element, {
+      // options
+    }).setView([48.858373738258386, 2.3518390365783475], 13);
 
-  return <div id="map"></div>;
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+
+    return map;
+  };
+
+  const removeMap = () => {
+    if (map.current) {
+      map.current.remove();
+      map.current = null;
+    }
+  };
+
+  useEffect(() => {
+    if (mapElement.current) {
+      map.current = initMap(mapElement.current);
+    } else {
+      removeMap();
+      console.error("Error");
+    }
+
+    return () => {
+      removeMap();
+    };
+  }, []);
+
+  return (
+    <div id="map" ref={mapElement}></div>
+  );
 };
 
 export default LeafletMap;
