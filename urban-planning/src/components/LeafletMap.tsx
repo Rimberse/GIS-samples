c16eda4386e51d6df9797b7e1d5e9dde35d1d247
Rimberse <kerimpltm@gmail.com>
2023-07-07 15:57:04 +0200

    Replace OpenStreetMaps tiles to Mapbox tiles

--- urban-planning/src/components/LeafletMap.tsx
+++ urban-planning/src/components/LeafletMap.tsx
@@ -4,17 +4,18 @@ import "leaflet/dist/leaflet.css";
 
 const LeafletMap = () => {
   let mapElement: RefObject<HTMLDivElement> = useRef(null);
-  let map = useRef<Map | null>();
+  let map = useRef<Map | null>(null);
 
   const initMap = (element: HTMLElement) => {
     const map: Map = L.map(element, {
       // options
     }).setView([48.858373738258386, 2.3518390365783475], 13);
 
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
       maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+      id: 'mapbox/streets-v11',
+      accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
     }).addTo(map);
 
     return map;
