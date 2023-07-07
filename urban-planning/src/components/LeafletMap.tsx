e508d80b907b9324968f37b870661a383d53ab67
Rimberse <kerimpltm@gmail.com>
2023-07-07 13:21:38 +0200

    Create Leaflet Map component, used to handle map features

---
+++ urban-planning/src/components/LeafletMap.tsx
@@ -0,0 +1,11 @@
+import React, { useRef } from "react";
+
+const LeafletMap = () => {
+  const map = useRef();
+
+  const init = () => {};
+
+  return <div id="map"></div>;
+};
+
+export default LeafletMap;
