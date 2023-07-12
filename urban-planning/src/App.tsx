58206a7d68232710f0748cbc9e14bb2c04ce7ef5
Rimberse <kerimpltm@gmail.com>
2023-07-12 15:16:30 +0200

    Implement popup display with GeoJSON properties upon click, test with one GeoJSON layer

--- urban-planning/src/App.tsx
+++ urban-planning/src/App.tsx
@@ -6,7 +6,7 @@ import { GeoJSON, Feature } from 'geojson';
 import geoJSON from './arrondissements.json';
 import LoadingButton from "@mui/lab/LoadingButton";
 import LocationCityIcon from '@mui/icons-material/LocationCity';
-import { PathOptions } from 'leaflet';
+import { Content, Layer, PathOptions, Popup } from 'leaflet';
 import { Layers } from './interfaces/LeafletLayers';
 
 function App() {
@@ -15,6 +15,35 @@ function App() {
   const [layers, setLayers] = useState({});
 
   const style = (feature?: Feature): PathOptions => feature!.properties!.c_ar % 2 === 0 ? {color: "#ff0000"} : {color: "#0000ff"};
+
+  const onEachFeature = (feature: Feature, layer: Layer) => {
+    if (feature!.properties && feature.properties!.l_ar && feature.properties!.l_aroff && feature.properties.perimetre && feature.properties.surface) {
+      const content = {
+        districtNumber: feature.properties.l_ar,
+        officialName: feature.properties.l_aroff,
+        perimeter: feature.properties.perimetre,
+        surface: feature.properties.surface
+      }
+
+      const popup: Array<String> = [];
+
+      popup.push(
+        "<b>Arrondissement:</b> ",
+        content.districtNumber,
+        "<br>",
+        "<b>Official name:</b> ",
+        content.officialName,
+        "<br>",
+        "<b>Perimeter:</b> ",
+        content.perimeter,
+        "<br>",
+        "<b>Surface:</b> ",
+        content.surface
+      );
+      
+      layer.bindPopup(popup.join(""));
+    }
+}
 
   return (
     <div className="App">
@@ -30,7 +59,7 @@ function App() {
           setLoading(true);
           
           setTimeout(() => {
-            const layer = leafletMap.current?.importGeoJSON(geoJSON as GeoJSON, style);
+            const layer = leafletMap.current?.importGeoJSON(geoJSON as GeoJSON, style, onEachFeature);
 
             if (layer) {
               const newLayers: Layers = {
--- urban-planning/src/interfaces/LeafletLayerCreation.tsx
+++ urban-planning/src/interfaces/LeafletLayerCreation.tsx
@@ -2,7 +2,7 @@ import { Layer, PathOptions } from "leaflet";
 import { GeoJSON, Feature } from "geojson";
 
 interface LeafletMapCreateLayers {
-  importGeoJSON(geoJSON: GeoJSON, style: (feature?: Feature) => PathOptions | PathOptions): Layer;
+  importGeoJSON(geoJSON: GeoJSON, style: (feature?: Feature) => PathOptions | PathOptions, onEachFeature: (feature: Feature, layer: Layer) => void): Layer;
 }
 
 export type { LeafletMapCreateLayers };
