225a73c83c82d53bcc05d385a7e25a0d65d37374
Rimberse <kerimpltm@gmail.com>
2023-07-26 20:38:47 +0200

    Add more template function to LeafletMap, dealin with TileLayer creation

--- boilerplate/src/interfaces/LeafletLayerOperations.tsx
+++ boilerplate/src/interfaces/LeafletLayerOperations.tsx
@@ -1,7 +1,10 @@
-import { Layer, LayerGroup, PathOptions, Icon, Marker, LatLngExpression, MarkerClusterGroup, IconOptions, Control, TileLayer } from "leaflet";
+import { Layer, LayerGroup, PathOptions, Icon, Marker, LatLngExpression, MarkerClusterGroup, IconOptions, Control, TileLayer, WMSOptions } from "leaflet";
 import { GeoJSON, Feature } from "geojson";
+import { WMTSLayerOptions } from "./LeafletLayers";
 
 interface LeafletMapOperations {
+  createTileLayer(URL: string, options?: WMTSLayerOptions): TileLayer;
+  createWMSTileLayer(WMSURL: string, WMSOptions?: WMSOptions): TileLayer;
   getMainTileLayer(): TileLayer;
   createControlLayer(baseLayers?: Control.LayersObject, overlays?: Control.LayersObject): void;
   addLayer(layer: Layer): boolean;
--- boilerplate/src/interfaces/LeafletLayers.tsx
+++ boilerplate/src/interfaces/LeafletLayers.tsx
@@ -1,9 +1,29 @@
-import { LayerGroup } from "leaflet";
+import { LayerGroup, TileLayer } from "leaflet";
+
+// Used to keep track of created TileLayers
+interface TileLayers {
+  tileLayer?: TileLayer
+  // more user-defines Tilelayers..
+}
 
 // Used to keep track of created layers
 interface Layers {
   layer?: LayerGroup;
   // more user-defines layers..
+}
+
+// Represents WMTS TileLayer options
+interface WMTSLayerOptions {
+  Layer?: string, 
+  TIME?: string, 
+  TileMatrixSet?: string, 
+  TileMatrix?: number, 
+  TileRow?: number, 
+  TileCol?: number, 
+  maxZoom?: number,
+  imageId?: string,
+  apiKey?: string,
+  bandsFormula?: string
 }
 
 // Used with Leaflet's Control Layer
@@ -13,5 +33,5 @@ enum LayersControl {
   // more user-defines mappings for layer names in Control pane
 }
 
-export type { Layers };
+export type { TileLayers, Layers, WMTSLayerOptions };
 export { LayersControl };
